fab7ef313903b527edd39b3a1d7fa2df5000f7ce
Hao Xiong <haoxiong@outlook.com>
2017-03-16 23:06:03 -0500

    make some fields package private

--- src/TextClassification.java
+++ src/TextClassification.java
@@ -15,8 +15,8 @@ import java.util.stream.Stream;
 
 public class TextClassification {
     private static final String[] STOP_WORDS = {"a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"};
-    public static final int HAM = 0;
-    public static final int SPAM = 1;
+    static final int HAM = 0;
+    static final int SPAM = 1;
     private static final String[] TRAIN_PATH = {"dataset/train/ham", "dataset/train/spam"};
     private static final String[] TEST_PATH = {"dataset/test/ham", "dataset/test/spam"};
 
@@ -362,8 +362,8 @@ public class TextClassification {
  */
 class TextVector {
     //key: the index of the word in dictionary, value the number of the word
-    public float[] features;
-    public int type;
+    float[] features;
+    int type;
 
     TextVector(float[] fts, int tp) {
         features = fts;
